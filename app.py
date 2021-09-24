6a25380525428c710b3580a51df5561b5deb927a
daniel <dani.ajala@yahoo.com>
2021-09-24 03:04:12 -0300

    crud

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-
 from flask import Flask, redirect, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] ="postgresql://postgres:12345@localhost/jogo"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:12345@localhost/jogo"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
 class Usuario(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement = True)
     nome = db.Column(db.String, nullable=False)
 
     def __init__(self, nome):
@@ -18,8 +17,8 @@ class Usuario(db.Model):
 
 @app.route("/")
 def lista():
-    all_data = Usuario.query.all()
-    return render_template("lista.html", usuario=all_data)
+    usuario = Usuario.query.all()
+    return render_template("lista.html", usuario=usuario)
 
 
 @app.route("/form")
@@ -37,20 +36,23 @@ def criar():
         return redirect(url_for("lista"))
 
 
-@app.route("/edit")
-def edit():
-
-    return render_template("edit.html")
-
-
-@app.route("/atualizar", methods=["POST"])
-def atualizar():
+@app.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit(id):
+    usuario = Usuario.query.get(id)
     if request.method == "POST":
-        nome = request.form['nome']
-        usuario = Usuario(nome)
-        db.session.add(usuario)
+        usuario.nome = request.form['nome']
         db.session.commit()
         return redirect(url_for("lista"))
+    return render_template("edit.html", usuario=usuario)
+
+
+@app.route("/delete/<int:id>", methods=["GET", "POST"])
+def delete(id):
+    usuario = Usuario.query.get(id)
+    db.session.delete(usuario)
+    db.session.commit()
+    return redirect(url_for("lista"))
+
 
 
 if __name__ == "__main__":
