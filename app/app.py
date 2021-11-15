f8dc000d5b5537268cdf281fd94b50c43c7be67e
daniel <dani.ajala@yahoo.com>
2021-11-15 00:17:45 -0300

    jogo

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:12345@localhost/jogo"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = ["jogo"]
 db = SQLAlchemy(app)
 
 
